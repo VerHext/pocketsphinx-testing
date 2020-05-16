b891ffdf745f76126903a4577ef1caf4a888d818
VerHext <johannes@waigel.me>
2020-05-16 03:37:04 +0200

    add code / add test files

--- engine-audio/src/main.rs
+++ engine-audio/src/main.rs
@@ -1,3 +1,40 @@
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+
+
+    println!("Init config for pocketsphinx");
+
+
+    let ps_config = pocketsphinx::CmdLn::init(true, &["pocketsphinx",
+        "-hmm", "/usr/local/share/pocketsphinx/model/en-us/en-us",
+        "-lm", "/usr/local/share/pocketsphinx/model/en-us/en-us.lm.bin",
+        "-dict", "/usr/local/share/pocketsphinx/model/en-us/cmudict-en-us.dict",
+        ]);
+    let ps_decoder = pocketsphinx::PsDecoder::init(ps_config.unwrap());
+
+
+
+    let mut f = File::open(&"./Audiospur.raw").unwrap();
+    let mut buffer = [0; 2048];
+    let mut samples : [i16; 1024];
+
+    ps_decoder.start_utt(Some("something"));
+    loop {    
+        let n = f.read(&mut buffer).unwrap();
+        if n == 0 {
+            break;
+        }
+        samples = unsafe {std::mem::transmute(buffer)};
+        ps_decoder.process_raw(&mut samples, false, false);
+    }
+    ps_decoder.end_utt();
+    match ps_decoder.get_hyp() {
+            None => println!("Not recognized"),
+            Some((hyp, _utt_id, _score)) => println!("Recognized: {} - SCORE {}", hyp, _score),
+    }
+
+
 }
+
